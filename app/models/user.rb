40a9b8e7027695d2736cdfb5a7ed4f7460f69ca0
Dylan Drop <dylandrop@gmail.com>
2017-02-04 13:15:52 -0500

    Add models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable
+
+  has_many :conversations, through: :conversations_users
+  has_many :messages
+
+  validates :name, :email, :encrypted_password, presence: true
 end
