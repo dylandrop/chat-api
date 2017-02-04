8f53a82d7ee7cedae254a50069c1c287a68f504f
Dylan Drop <dylandrop@gmail.com>
2017-02-04 13:27:43 -0500

    Add some basic model tests

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,4 +1,22 @@
 require "rails_helper"
 
 describe Message do
+  context 'validations' do
+    let(:message) { Message.new }
+
+    describe 'content presence checks' do
+      it 'fails for blank content' do
+        message.valid?
+        expect(message.errors[:content]).not_to be_blank
+      end
+    end
+
+    describe 'relation presence checks' do
+      it 'fails for blank content' do
+        message.valid?
+        expect(message.errors[:user]).not_to be_blank
+        expect(message.errors[:conversation]).not_to be_blank
+      end
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,16 @@
 require "rails_helper"
 
 describe User do
+  context 'validations' do
+    let(:user) { User.new }
+
+    describe 'name, email, password presence checks' do
+      it 'fails for blank content' do
+        user.valid?
+        expect(user.errors[:email]).not_to be_blank
+        expect(user.errors[:encrypted_password]).not_to be_blank
+        expect(user.errors[:name]).not_to be_blank
+      end
+    end
+  end
 end
