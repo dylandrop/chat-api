f19b2043395d6e7c4f07f6cdb4807f16e956dbfc
Dylan Drop <dylandrop@gmail.com>
2017-02-05 11:05:12 -0500

    Productionize with Postgres gem

--- Gemfile
+++ Gemfile
@@ -8,13 +8,12 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.1'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 
 gem 'bcrypt'
 gem 'active_model_serializers'
+gem 'pg'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170204193612) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "conversations", force: :cascade do |t|
     t.string   "subject"
     t.datetime "created_at", null: false
@@ -21,17 +24,17 @@ ActiveRecord::Schema.define(version: 20170204193612) do
   create_table "conversations_users", id: false, force: :cascade do |t|
     t.integer "user_id",         null: false
     t.integer "conversation_id", null: false
-    t.index ["user_id", "conversation_id"], name: "index_conversations_users_on_user_id_and_conversation_id"
+    t.index ["user_id", "conversation_id"], name: "index_conversations_users_on_user_id_and_conversation_id", using: :btree
   end
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.integer  "conversation_id"
-    t.integer  "user_id"
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
+    t.integer  "conversation_id", null: false
+    t.integer  "user_id",         null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,8 +44,10 @@ ActiveRecord::Schema.define(version: 20170204193612) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "api_auth_token"
-    t.index ["api_auth_token"], name: "index_users_on_api_auth_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["api_auth_token"], name: "index_users_on_api_auth_token", unique: true, using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
